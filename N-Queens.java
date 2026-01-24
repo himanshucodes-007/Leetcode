e6500e21c007815e644e169176ee5a1caf3a334f
Himanshu Yadav <himanshucodes.dev@gmail.com>
2026-01-24 17:57:37 +0530

    [51] [Time Beats: 13.42%] [Memory Beats: 54.70%] - LeetPush

--- N-Queens.java
+++ N-Queens.java
@@ -4,9 +4,9 @@
 4    public List<List<String>> solveNQueens(int n) {
 5        if (n == 0)
 6            return result;
-7        
+7
 8        List<String> board = new ArrayList<>();
-9        // For n = 3, board = {"...", "...", "..."} initially
+9        // For, n = 3, board = {"...", "...", "..."} initially
 10        for (int i = 0; i < n; i++) {
 11            StringBuilder row = new StringBuilder();
 12            for (int j = 0; j < n; j++) {
@@ -15,56 +15,55 @@
 15            board.add(row.toString());
 16        }
 17
-18        solve(board, 0);
-19        
-20        return result;
-21    }
-22
-23    private boolean isValid(List<String> board, int row, int col) {
-24        // Look for up
-25        for (int i = row; i >= 0; i--) {
-26            if (board.get(i).charAt(col) == 'Q')
-27                return false;
-28        }
-29
-30        // Check left diagonal upwards
-31        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
-32            if (board.get(i).charAt(j) == 'Q')
-33                return false;
-34        }
-35
-36        // Check right diagonal upwards
-37        for (int i = row, j = col; i >= 0 && j < board.size(); i--, j++) {
-38            if (board.get(i).charAt(j) == 'Q')
-39                return false;
-40        }
+18        int startRow = 0;
+19        HashSet<Integer> cols = new HashSet<>();
+20        HashSet<Integer> diags = new HashSet<>();
+21        HashSet<Integer> antiDiags = new HashSet<>();
+22        solve(board, startRow, cols, diags, antiDiags);
+23
+24        return result;
+25    }
+26
+27    private void solve(List<String> board, int row, HashSet<Integer> cols, HashSet<Integer> diags, HashSet<Integer> antiDiags) {
+28        if (row == board.size()) {
+29            result.add(new ArrayList<>(board));
+30            return;
+31        }
+32
+33        /*
+34         * For a square (i, j) : Diagonally (i-j) is constant Anti diagonally (i+j) is
+35         * constant
+36         * 
+37         * We can use this to find which square (i, j) has a risk of being attacked
+38         * by another queen placed already in 'diagonal', or 'anti-diagonal' or
+39         * 'column'
+40         */
 41
-42        return true;
-43    }
-44
-45    private void solve(List<String> board, int row) {
-46        if (row == board.size()) {
-47            result.add(new ArrayList<>(board));
-48            return;
-49        }
-50
-51        // Place one queen at every row and check before placing
-52        // in every direction where there is a risk of being attacked
-53        // i.e., up, diagonally because we are placing queens from
-54        // the top row to the bottom row, so we need to check if we put a queen
-55        // vertically up in some row or diagonally upwards in some row
-56
-57        for (int i = 0; i < board.size(); i++) {
-58            if (isValid(board, row, i)) {
-59                StringBuilder newRow = new StringBuilder(board.get(row));
-60                newRow.setCharAt(i, 'Q');
-61                board.set(row, newRow.toString());
-62
-63                solve(board, row + 1);
-64
-65                newRow.setCharAt(i, '.');
-66                board.set(row, newRow.toString());
-67            }
-68        }
-69    }
-70}
+42        for (int col = 0; col < board.size(); col++) {
+43            int diagId = row - col;
+44            int antiDiagId = row + col;
+45
+46            /*
+47             * If the col, or diagonal or anti_diagonal are used means one of them has a
+48             * Queen placed already which can attack, so look for other column
+49             */
+50            if (cols.contains(col) || diags.contains(diagId) || antiDiags.contains(antiDiagId))
+51                continue;
+52
+53            cols.add(col);
+54            diags.add(diagId);
+55            antiDiags.add(antiDiagId);
+56            StringBuilder newRow = new StringBuilder(board.get(row));
+57            newRow.setCharAt(col, 'Q');
+58            board.set(row, newRow.toString());
+59
+60            solve(board, row + 1, cols, diags, antiDiags);
+61
+62            cols.remove(col);
+63            diags.remove(diagId);
+64            antiDiags.remove(antiDiagId);
+65            newRow.setCharAt(col, '.');
+66            board.set(row, newRow.toString());
+67        }
+68    }
+69}
