8de0985eabcb6b915e2ab126f53f2f42b963d168
Himanshu Yadav <himanshucodes.dev@gmail.com>
2026-05-22 13:49:57 +0530

    [33] [Time Beats: 100.00%] [Memory Beats: 45.24%] - LeetPush

--- Search-in-Rotated-Sorted-Array.java
+++ Search-in-Rotated-Sorted-Array.java
@@ -1,32 +1,29 @@
 1class Solution {
 2    public int search(int[] nums, int target) {
-3        int low =0;
-4        int high = nums.length-1;
+3        int n = nums.length;
+4        int lo = 0, hi = n-1;
 5
-6        while(low<=high){
-7            int mid = (low+high)/2;
+6        while(lo<hi){
+7            int mid = (lo+hi)/2;
 8
-9            if(nums[mid] == target) return mid;
-10
-11            if(nums[low] <= nums[mid]){
-12                if(nums[low] <= target && target < nums[mid]){
-13                    high = mid-1;
-14                }
-15                else{
-16                    low = mid+1;
-17                }
-18            }
-19
-20            else{
-21                if(nums[mid]<target && target <= nums[high]){
-22                    low = mid+1;
-23                }
-24                else{
-25                    high = mid-1;
-26                }
-27            }
-28            
-29        }
-30        return -1;
-31    }
-32}
+9            if(nums[mid] > nums[n-1]) lo= mid+1;
+10            else hi = mid;
+11        }
+12
+13        int rot = lo;
+14
+15        lo=0; hi = n-1;
+16
+17        while(lo <= hi){
+18            int mid = (lo+hi)/2;
+19            int real = (mid+rot)%n;
+20
+21            if(nums[real] == target) return real;
+22
+23            if(nums[real] < target) lo = mid+1;
+24            else hi = mid-1;
+25        }
+26
+27        return -1;
+28    }
+29}
