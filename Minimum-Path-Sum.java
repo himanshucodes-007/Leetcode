c04a06e030af144d2361e3ee1edaca24cd7212ec
Himanshu Yadav <himanshucodes.dev@gmail.com>
2026-05-26 16:44:45 +0530

    [64] [Time Beats: 10.43%] [Memory Beats: 70.53%] - LeetPush

--- Minimum-Path-Sum.java
+++ Minimum-Path-Sum.java
@@ -3,25 +3,31 @@
 3        int m = grid.length;
 4        int n = grid[0].length;
 5
-6        int[][] dp = new int[m][n];
+6        int[] prev = new int[n];
 7
-8        dp[0][0] = grid[0][0];
-9
-10        for(int i =0;i<m;i++){
+8        for(int i=0;i<m;i++){
+9            int[] curr = new int[n];
+10
 11            for(int j=0;j<n;j++){
-12                if(i==0 && j ==0) continue;
+12                if(i==0 && j ==0){
 13
-14                int up = Integer.MAX_VALUE;
-15                int left = Integer.MAX_VALUE;
-16
-17                if(i>0) up = dp[i-1][j];
-18
-19                if(j>0) left = dp[i][j-1];
+14                    curr[j] = grid[0][0];
+15                    continue;
+16                }
+17
+18                int up = Integer.MAX_VALUE;
+19                int left = Integer.MAX_VALUE;
 20
-21                dp[i][j] = grid[i][j] + Math.min(up,left);
-22            }
-23        }
+21                if(i>0) up= prev[j];
+22
+23                if(j>0) left = curr[j-1];
 24
-25        return dp[m-1][n-1];
-26    }
-27}
+25                curr[j] =  grid[i][j]+Math.min(up,left);
+26            }
+27
+28            prev = curr;
+29        }
+30
+31        return prev[n-1];
+32    }
+33}
