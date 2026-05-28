c2cc2c80cf346772a246efbbadb01f667b69a9d3
Himanshu Yadav <himanshucodes.dev@gmail.com>
2026-05-28 19:06:07 +0530

    [494] [Time Beats: 78.67%] [Memory Beats: 39.74%] - LeetPush

--- Target-Sum.java
+++ Target-Sum.java
@@ -10,32 +10,40 @@
 10            return 0;
 11        }
 12
-13        int newTarget = (totalSum+target)/2;
+13        int tar = (totalSum+target)/2;
 14
-15        return solve(nums.length-1,newTarget,nums);
-16    }
-17
-18    public int solve(int i, int target,int[] nums){
-19        if(i==0){
-20            if(target ==0 && nums[0] == 0){
-21                return 2;
-22            }
+15        int n = nums.length;
+16
+17        int[][] dp = new int[n][tar+1];
+18
+19        //base case
+20        if(nums[0] == 0){
+21            dp[0][0] = 2;
+22        }
 23
-24            if(target ==0 || target == nums[0]){
-25                return 1;
-26            }
+24        else{
+25            dp[0][0] = 1;
+26        }
 27
-28            return 0;
-29        }
-30        int notTake = solve(i-1,target,nums);
-31        int take =0;
-32
-33        if(nums[i] <= target){
+28        if(nums[0] != 0 && nums[0] <= tar){
+29            dp[0][nums[0]] =1;
+30        }
+31
+32        for(int i=1;i<n;i++){
+33            for(int t=0;t<= tar;t++){
 34
-35            take = solve(i-1,target-nums[i],nums);
-36        }
-37
+35                int notTake = dp[i-1][t];
+36
+37                int take =0;
 38
-39        return take+notTake;
-40    }
-41}
+39                if(nums[i] <= t){
+40                    take = dp[i-1][t-nums[i]];
+41                }
+42
+43                dp[i][t] = take+notTake;
+44            }
+45        }
+46
+47        return dp[n-1][tar];
+48    }
+49}
