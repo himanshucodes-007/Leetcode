970f4e4eb83d8a6d50e9dbf86174157952cdba5a
Himanshu Yadav <himanshucodes.dev@gmail.com>
2026-05-28 19:19:27 +0530

    [494] [Time Beats: 84.67%] [Memory Beats: 45.61%] - LeetPush

--- Target-Sum.java
+++ Target-Sum.java
@@ -2,48 +2,33 @@
 2    public int findTargetSumWays(int[] nums, int target) {
 3        int totalSum =0;
 4
-5        for(int num: nums){
-6            totalSum += num;
-7        }
+5        for(int num : nums) totalSum += num;
+6
+7        if(totalSum < Math.abs(target) || (totalSum + target)% 2  != 0) return 0;
 8
-9        if(totalSum < Math.abs(target) || (totalSum+target)%2 != 0 ){
-10            return 0;
-11        }
+9        int tar = (totalSum + target) /2;
+10
+11        int[] prev = new int[tar+1];
 12
-13        int tar = (totalSum+target)/2;
+13        prev[0]= (nums[0] == 0) ? 2:1;
 14
-15        int n = nums.length;
+15        if(nums[0] != 0 && nums[0] <= tar) prev[nums[0]] =1;
 16
-17        int[][] dp = new int[n][tar+1];
-18
-19        //base case
-20        if(nums[0] == 0){
-21            dp[0][0] = 2;
-22        }
-23
-24        else{
-25            dp[0][0] = 1;
-26        }
-27
-28        if(nums[0] != 0 && nums[0] <= tar){
-29            dp[0][nums[0]] =1;
+17        for(int i =1; i <nums.length;i++){
+18            int[] curr = new int[tar+1];
+19
+20            for(int t =0; t <= tar; t++){
+21                int notTake= prev[t];
+22
+23                int take = 0;
+24
+25                if(nums[i] <= t) take = prev[t - nums[i]];
+26                curr[t] = take+notTake;
+27            }
+28
+29            prev = curr;
 30        }
 31
-32        for(int i=1;i<n;i++){
-33            for(int t=0;t<= tar;t++){
-34
-35                int notTake = dp[i-1][t];
-36
-37                int take =0;
-38
-39                if(nums[i] <= t){
-40                    take = dp[i-1][t-nums[i]];
-41                }
-42
-43                dp[i][t] = take+notTake;
-44            }
-45        }
-46
-47        return dp[n-1][tar];
-48    }
-49}
+32        return prev[tar];
+33    }
+34}
