bcd062242491d08d7a7a6c0b0608d11f3afe521d
Himanshu Yadav <himanshucodes.dev@gmail.com>
2026-05-29 16:49:45 +0530

    [322] [Time Beats: 96.22%] [Memory Beats: 70.33%] - LeetPush

--- Coin-Change.java
+++ Coin-Change.java
@@ -1,38 +1,16 @@
 1class Solution {
 2    public int coinChange(int[] coins, int amount) {
-3        int n = coins.length;
+3        int[] dp = new int[amount+1];
 4
-5        int[][] dp = new int[n][amount+1];
-6        
-7        for(int t =0; t <= amount; t++){
-8            if(t% coins[0] ==0){
-9                dp[0][t] = t/coins[0];
-10            }
-11            else{
-12                dp[0][t] = Integer.MAX_VALUE;
-13            }
-14        }
-15
-16        for(int i =1; i<n; i++){
-17            for(int target =0; target <= amount; target++){
-18
-19                int notTake = dp[i-1][target];
-20
-21                int take = Integer.MAX_VALUE;
-22                if (coins[i] <= target) {
-23                    int res = dp[i][target - coins[i]];
-24
-25                    if (res != Integer.MAX_VALUE) {
-26                        take = 1 + res;
-27                    }
-28                }
-29
-30                dp[i][target] = Math.min(take,notTake);
-31
-32            }
-33        }
-34
-35        int ans = dp[n-1][amount];
-36        return ans >= Integer.MAX_VALUE ? -1 : ans;
-37    }
-38}
+5        Arrays.fill(dp, amount+1);
+6        dp[0] =0;
+7
+8        for(int coin : coins){
+9            for(int target = coin; target <= amount; target++){
+10                dp[target] = Math.min(dp[target], 1+dp[target-coin]);
+11            }
+12        }
+13
+14        return dp[amount] > amount ? -1: dp[amount];
+15    }
+16}
