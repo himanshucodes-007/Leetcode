8f26e9196a58fbee112df7ae3b6f9bab2a868bb4
Himanshu Yadav <himanshucodes.dev@gmail.com>
2026-06-03 16:49:33 +0530

    [1143] [Time Beats: 93.81%] [Memory Beats: 94.10%] - LeetPush

--- Longest-Common-Subsequence.java
+++ Longest-Common-Subsequence.java
@@ -3,19 +3,24 @@
 3        int n = text1.length();
 4        int m = text2.length();
 5
-6        int[][] dp = new int[n+1][m+1];
+6        int[] prev = new int[m+1];
 7
 8        for(int i=1; i <= n; i++){
-9            for(int j=1;j<=m; j++){
-10                if(text1.charAt(i-1) == text2.charAt(j-1)){
-11                    dp[i][j] = 1+ dp[i-1][j-1];
-12                }
-13                else{
-14                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+9
+10            int[] curr = new int[m+1];
+11
+12            for(int j=1;j<=m; j++){
+13                if(text1.charAt(i-1) == text2.charAt(j-1)){
+14                    curr[j] = 1+ prev[j-1];
 15                }
-16            }
-17        }
-18
-19        return dp[n][m];
-20    }
-21}
+16                else{
+17                    curr[j] = Math.max(prev[j], curr[j-1]);
+18                }
+19            }
+20
+21            prev = curr;
+22        }
+23
+24        return prev[m];
+25    }
+26}
