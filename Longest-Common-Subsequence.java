2623917720937c0b6c981d0b0eedcd5c35adcef8
Himanshu Yadav <himanshucodes.dev@gmail.com>
2026-06-03 16:39:34 +0530

    [1143] [Time Beats: 78.30%] [Memory Beats: 50.09%] - LeetPush

--- Longest-Common-Subsequence.java
+++ Longest-Common-Subsequence.java
@@ -1,25 +1,21 @@
 1class Solution {
-2    int[][] dp;
-3    public int longestCommonSubsequence(String text1, String text2) {
-4        int n = text1.length();
-5        int m = text2.length();
-6
-7        dp = new int[n][m];
-8
-9        for(int[] row: dp) Arrays.fill(row, -1);
-10
-11        return solve(n-1, m-1, text1, text2);
-12    }
-13
-14    private int solve(int i, int j, String s1, String s2){
-15        if(i<0 || j<0) return 0;
-16
-17        if(dp[i][j] != -1) return dp[i][j];
+2    public int longestCommonSubsequence(String text1, String text2) {
+3        int n = text1.length();
+4        int m = text2.length();
+5
+6        int[][] dp = new int[n+1][m+1];
+7
+8        for(int i=1; i <= n; i++){
+9            for(int j=1;j<=m; j++){
+10                if(text1.charAt(i-1) == text2.charAt(j-1)){
+11                    dp[i][j] = 1+ dp[i-1][j-1];
+12                }
+13                else{
+14                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+15                }
+16            }
+17        }
 18
-19        if(s1.charAt(i) == s2.charAt(j)){
-20            return 1+ solve(i-1, j-1, s1, s2);
-21        }
-22
-23        return dp[i][j] = Math.max(solve(i-1, j, s1, s2), solve(i, j-1, s1, s2));
-24    }
-25}
+19        return dp[n][m];
+20    }
+21}
