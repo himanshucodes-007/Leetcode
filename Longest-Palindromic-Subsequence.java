8f8c0d896f1d6854315d03ac366cffd791f30204
Himanshu Yadav <himanshucodes.dev@gmail.com>
2026-06-03 21:51:24 +0530

    [516] [Time Beats: 20.64%] [Memory Beats: 61.58%] - LeetPush

--- Longest-Palindromic-Subsequence.java
+++ Longest-Palindromic-Subsequence.java
@@ -1,26 +1,21 @@
 1class Solution {
-2    int[][] dp;
-3    public int longestPalindromeSubseq(String s) {
-4        String rev = new StringBuilder(s).reverse().toString();
-5        int n = s.length();
-6
-7        dp = new int[n][n];
-8        for(int[] row: dp){
-9            Arrays.fill(row,-1);
-10        }
-11
-12        return solve(n-1,n-1,s,rev);
-13    }
-14
-15    public int solve(int i , int j , String s1, String s2){
-16        if(i< 0 || j < 0) return 0;
-17
-18        if(dp[i][j] != -1) return dp[i][j];
-19
-20        if(s1.charAt(i) == s2.charAt(j)){
-21            return 1+solve(i-1,j-1,s1,s2);
-22        }
-23
-24        return dp[i][j] = Math.max(solve(i-1,j,s1,s2),solve(i,j-1,s1,s2));
-25    }
-26}
+2    public int longestPalindromeSubseq(String s) {
+3        String rev = new StringBuilder(s).reverse().toString();
+4        int n = s.length();
+5
+6        int[][] dp = new int[n+1][n+1];
+7        
+8        for(int i=1; i<=n; i++){
+9            for(int j = 1; j<=n; j++){
+10                if(s.charAt(i-1) == rev.charAt(j-1)){
+11                    dp[i][j] = 1+dp[i-1][j-1];
+12                }
+13                else{
+14                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+15                }
+16            }
+17        }
+18
+19        return dp[n][n];
+20    }
+21}
