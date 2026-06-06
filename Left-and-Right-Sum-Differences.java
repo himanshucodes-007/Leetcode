15e3376dcd50e6352a487d83459a0e07ec97650e
Himanshu Yadav <himanshucodes.dev@gmail.com>
2026-06-06 09:45:34 +0530

    [2574] [Time Beats: 98.84%] [Memory Beats: 96.27%] - LeetPush

--- Left-and-Right-Sum-Differences.java
+++ Left-and-Right-Sum-Differences.java
@@ -1,35 +1,22 @@
 1class Solution {
 2    public int[] leftRightDifference(int[] nums) {
-3        int n = nums.length;
-4        int[] ans = new int[n];
-5
-6        if(n==1) return ans;
+3
+4        int n= nums.length;
+5        int[] ans = new int[n];
+6
 7
-8        int[] leftarr = new int[n];
-9        for(int i=0;i<n;i++){
-10            if(i==0){
-11                leftarr[i] =0;
-12            }
-13            else{
-14                leftarr[i] = leftarr[i-1]+nums[i-1]; 
-15            }
-16        }
-17
-18        int[] rightarr = new int[n];
+8        int left =0;
+9        for(int i=0; i<n;i++){
+10            ans[i] = left;
+11            left += nums[i];
+12        }
+13
+14        int right =0;
+15        for(int i = n-1; i>=0; i--){
+16            ans[i] = Math.abs(ans[i]-right);
+17            right += nums[i];
+18        }
 19
-20        for(int i=n-1; i>=0; i--){
-21            if(i==n-1){
-22                rightarr[i] = 0;
-23            }
-24            else{
-25                rightarr[i] = rightarr[i+1]+nums[i+1];
-26            }
-27        }
-28
-29        for(int i=0;i<n;i++){
-30            ans[i] = Math.abs(leftarr[i]-rightarr[i]);
-31        }
-32
-33        return ans;
-34    }
-35}
+20        return ans;
+21    }
+22}
