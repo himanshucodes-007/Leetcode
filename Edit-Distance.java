f3c80e2ee1d9bb7ddb70793b9baeef074aba10d1
Himanshu Yadav <himanshucodes.dev@gmail.com>
2026-06-07 20:13:54 +0530

    [72] [Time Beats: 69.54%] [Memory Beats: 5.97%] - LeetPush

--- Edit-Distance.java
+++ Edit-Distance.java
@@ -1,35 +1,29 @@
 1class Solution {
-2    int[][] dp;
-3    public int minDistance(String word1, String word2) {
-4        int n = word1.length();
-5        int m = word2.length();
-6
-7        dp = new int[n][m];
-8
-9        for(int[] row: dp){
-10            Arrays.fill(row,-1);
-11        }
-12
-13        return solve(n-1,m-1,word1,word2);
-14    }
-15
-16    private int solve(int i, int j, String s1, String s2){
-17        if(i<0) return j+1;
-18        if(j<0) return i+1;
-19
-20        if(s1.charAt(i) == s2.charAt(j)){
-21            return solve(i-1,j-1,s1,s2);
-22        }
-23
-24        if(dp[i][j] != -1){
-25            return dp[i][j];
-26        }
-27
-28
-29        int insert = 1+solve(i,j-1,s1,s2);
-30        int delete = 1+solve(i-1,j,s1,s2);
-31        int replace = 1+solve(i-1,j-1,s1,s2);
-32
-33        return dp[i][j] = Math.min(insert, Math.min(delete, replace));
-34    }
-35}
+2    public int minDistance(String word1, String word2) {
+3        int n = word1.length();
+4        int m = word2.length();
+5
+6        int[][] dp = new int[n+1][m+1];
+7
+8        for(int i=0; i<=n; i++) dp[i][0] = i;
+9        for(int j=0; j<=m; j++) dp[0][j] = j;
+10
+11        for(int i=1; i<=n; i++){
+12            for(int j=1; j<=m; j++){
+13                if(word1.charAt(i-1) == word2.charAt(j-1)){
+14                    dp[i][j] = dp[i-1][j-1];
+15                }
+16
+17                else{
+18                    int insert = dp[i][j-1];
+19                    int delete = dp[i-1][j];
+20                    int replace = dp[i-1][j-1];
+21
+22                    dp[i][j] = 1+Math.min(insert,Math.min(delete,replace));
+23                }
+24            }
+25        }
+26
+27        return dp[n][m];
+28    }
+29}
