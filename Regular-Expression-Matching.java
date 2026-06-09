a005ac87860b71fffd43ca89dbd5511fc93c6611
Himanshu Yadav <himanshucodes.dev@gmail.com>
2026-06-09 16:36:24 +0530

    [10] [Time Beats: 51.04%] [Memory Beats: 48.83%] - LeetPush

--- Regular-Expression-Matching.java
+++ Regular-Expression-Matching.java
@@ -1,63 +1,33 @@
 1class Solution {
-2    int[][] dp;
-3    public boolean isMatch(String s, String p) {
-4        int n = s.length();
-5        int m = p.length();
-6
-7        dp = new int[n][m];
-8
-9        for(int[] row: dp){
-10            Arrays.fill(row,-1);
-11        }
-12
-13        return solve(n-1,m-1,s,p);
-14    }
+2    public boolean isMatch(String s, String p) {
+3        int n = s.length();
+4        int m = p.length();
+5
+6        boolean[][] dp = new boolean[n+1][m+1];
+7
+8        dp[0][0] = true;
+9
+10        for(int j=2; j<=m; j++){
+11            if(p.charAt(j-1) == '*'){
+12                dp[0][j] = dp[0][j-2];
+13            }
+14        }
 15
-16    private boolean solve(int i, int j, String s1, String s2){
-17        if(i<0 && j <0){
-18            return true;
-19        }
-20
-21        if(j<0){
-22            return false;
-23        }
-24
-25        if(i<0){
-26            for(int k=j; k>=0;k-=2){
-27                if(s2.charAt(k) !='*'){
-28                    return false;
-29                }
-30            }
-31
-32            return true;
-33        }
-34
-35        if(dp[i][j] != -1){
-36            return dp[i][j] == 1;
-37        }
-38
-39        boolean ans;
-40
-41        if(s1.charAt(i) == s2 .charAt(j) || s2.charAt(j) == '.'){
-42            ans = solve(i-1,j-1,s1,s2);
-43        }
-44
-45        else if(s2.charAt(j) == '*'){
-46            boolean notTake = solve(i,j-2,s1,s2);
-47            boolean take = false;
-48
-49            if(s1.charAt(i) == s2 .charAt(j-1) || s2.charAt(j-1) == '.'){
-50                take = solve(i-1,j,s1,s2);
-51            }
-52
-53            ans = take || notTake;
-54        }
-55        else{
-56            ans = false;
-57        }
-58
-59        dp[i][j] = ans ? 1: 0;
-60
-61        return ans;
-62    }
-63}
+16        for(int i =1; i<=n; i++){
+17            for(int j=1; j<= m ; j++){
+18                if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '.'){
+19                    dp[i][j] = dp[i-1][j-1];
+20                }
+21                else if(p.charAt(j-1) =='*'){
+22                    dp[i][j] = dp[i][j-2];
+23
+24                    if(s.charAt(i-1) == p.charAt(j-2) || p.charAt(j-2) == '.'){
+25                        dp[i][j] = dp[i][j] || dp[i-1][j];
+26                    }
+27                }
+28            }
+29        }
+30
+31        return dp[n][m];
+32    }
+33}
